90b00c318af01d832ebe48c8d02100f27ac04db1
vord <m.iqbal.putra02@gmail.com>
2025-02-20 10:46:58 +0700

    20 Feb

--- C/Data_Structure/doubleLinkedlist.cpp
+++ C/Data_Structure/doubleLinkedlist.cpp
@@ -10,8 +10,10 @@ struct Node{
 
 struct Node *newNode(int data){
     struct Node *newNode = (struct Node*)malloc(sizeof(struct Node) * 1);
+    
     newNode->data = data;
     newNode->next = newNode->prev = NULL;
+
     return newNode;
 }
 
@@ -42,10 +44,69 @@ void pushMid(Node *newNode){
     if(head == NULL){
         head = tail = newNode;
     }
+    else if(head->data > newNode->data){
+        pushHead(newNode);
+    }
+    else if(tail->data < newNode->data){
+        pushTail(newNode);
+    }
     else {
-        tail->next = newNode;
-        newNode->prev = tail;
-        tail = newNode;
+        Node *curr = head;
+        while( (curr->next != NULL) && (curr->next->data < newNode->data) ){
+            curr = curr->next;
+        }
+        newNode->next = curr->next;
+        curr->next->prev = newNode;
+        curr->next = newNode; // pointer hanya bisa menunjuk satu arah
+        newNode->prev = curr;
+    }
+}
+
+void popHead(){
+
+    if(head == NULL){
+        return;
+    }
+    else if(head == tail){
+        free(head);
+        head = tail = NULL;
+    }
+    else {
+        Node *temp = head;
+
+        head = head->next;
+        head->prev = NULL;
+        free(temp);
+    }
+}
+
+void popTail(){
+
+    if(head == NULL){
+        return;
+    }
+    else if(head == tail){
+        free(head);
+        head = tail = NULL;
+    }
+    else {
+        Node *temp = tail;
+
+        tail = tail->prev;
+        tail->next = NULL;
+        free(temp);
+    }
+}
+
+void popMid(int data){
+    if(head == NULL){
+        return;
+    }
+    else if(head->data == data){
+        popHead();
+    }
+    else if(tail->data == data){
+        popTail();
     }
 }
 int main(){
