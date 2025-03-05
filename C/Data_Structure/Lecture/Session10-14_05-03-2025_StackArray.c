08f1573683f284b59e648cda9ff6e04eb0cdecf7
vord <m.iqbal.putra02@gmail.com>
2025-03-05 09:56:03 +0700

    5 March

---
+++ C/Data_Structure/Lecture/Session10-14_05-03-2025_StackArray.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+
+#define MAX 5
+
+int stack[MAX], top = -1;
+
+void push(int value){
+    if(top == MAX - 1){
+        printf("Stack overflow, tidak bisa menambahkan %d\n", value);
+    }
+    else {
+        top++; // geser TOP ke atas
+        stack[top] = value;
+        printf("Menambahkan %d ke dalam stack\n", value);
+    }
+}
+
+void display(){
+    if(top == -1){
+        printf("Stack kosong\n");
+    }
+    else {
+        printf("Stack: \n");
+
+        for(int i = top; i >= 0; i--){
+            printf("%d ", stack[i]);
+            printf("\n");
+        }
+    }
+}
+
+int main(){
+
+    push(10);
+    push(20);
+    push(30);
+    push(40);
+    push(50);
+
+    display();
+
+
+
+
+
+
+
+
+    return 0;
+}
+
