774108d75f1565d7071608fee877b59d07a78363
Iqbal Saputra <m.iqbal.putra02@gmail.com>
2025-03-19 12:14:16 +0700

    push

--- C/Data_Structure/doubleLinkedlist.cpp
+++ C/Data_Structure/doubleLinkedlist.cpp
@@ -1,122 +1,193 @@
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
+
 
 struct Node{
-    int data;
-    struct Node *next, *prev;
+	
+	int data;
+	char nama[101];
+	struct Node *next, *prev;
+	
+}*head, *tail;
 
-} *head, *tail;
-
-struct Node *newNode(int data){
-    struct Node *newNode = (struct Node*)malloc(sizeof(struct Node) * 1);
-    
-    newNode->data = data;
-    newNode->next = newNode->prev = NULL;
-
-    return newNode;
+struct Node *newNode(int data, char nama[101]){
+	struct Node *temp = (struct Node*)malloc(sizeof(struct Node));
+	
+	temp->data = data;
+	strcpy(temp->nama, nama);
+	
+	temp->next = temp->prev = NULL;
+	
+	return temp;
 }
 
 void pushHead(struct Node *newNode){
-    if(head == NULL){
-        head = tail = newNode;
-    }
-    else{
-        newNode->next = head;
-        head->prev = newNode;
-        head = newNode;
-    }
+	
+	if(head == NULL){
+		head = tail = newNode;
+	}
+	else {
+		newNode->next = head;
+		head->prev = newNode;
+		head = newNode;
+	}
 }
 
 void pushTail(struct Node *newNode){
-    if(head == NULL){
-        head = tail = newNode;
-    }
-    else {
-        tail->next = newNode;
-        newNode->prev = tail;
-        tail = newNode;
-    }
+	
+	if(head == NULL){
+		head = tail = newNode;
+	}
+	else {
+		tail->next = newNode;
+		newNode->prev = tail;
+		tail = newNode;
+	}
 }
 
-void pushMid(Node *newNode){
-    
-    if(head == NULL){
-        head = tail = newNode;
-    }
-    else if(head->data > newNode->data){
-        pushHead(newNode);
-    }
-    else if(tail->data < newNode->data){
-        pushTail(newNode);
-    }
-    else {
-        Node *curr = head;
-        while( (curr->next != NULL) && (curr->next->data < newNode->data) ){
-            curr = curr->next;
-        }
-        newNode->next = curr->next;
-        if(curr->next != NULL){
-            curr->next->prev = newNode;
-        }
-        curr->next = newNode; // pointer hanya bisa menunjuk satu arah
-        newNode->prev = curr;
-    }
+void pushMid(struct Node *newNode){
+
+	if(head == NULL){
+		head = tail = newNode;
+	}
+	else if(strcmp(head->nama, newNode->nama) > 0){
+		pushHead(newNode);
+	}
+	else if(strcmp(tail->nama, newNode->nama) < 0){
+		pushTail(newNode);
+	}
+	else{
+		struct Node *curr = head;
+		while( (curr->next != NULL) && strcmp(curr->next->nama, newNode->nama) < 0 ){
+			curr = curr->next;
+		}
+		newNode->next = curr->next;
+		if(curr->next != NULL){
+			curr->next->prev = newNode;
+		}
+		curr->next = newNode;
+		newNode->prev = curr;
+	}
 }
 
 void popHead(){
-
-    if(head == NULL){
-        return;
-    }
-    else if(head == tail){
-        free(head);
-        head = tail = NULL;
-    }
-    else {
-        Node *temp = head;
-
-        head = head->next;
-        head->prev = NULL;
-        free(temp);
-    }
+	
+	if(head == NULL){
+		return;
+	}
+	else if(head == tail){
+		free(head);
+		head = tail = NULL;
+	}
+	else {
+		struct Node *temp = head;
+		head = head->next;
+		head->prev = NULL;
+		free(temp);
+	}
+	
 }
 
 void popTail(){
-
-    if(head == NULL){
-        return;
-    }
-    else if(head == tail){
-        free(head);
-        head = tail = NULL;
-    }
-    else {
-        Node *temp = tail;
-
-        tail = tail->prev;
-        tail->next = NULL;
-        free(temp);
-    }
+	
+	if(head == NULL){
+		return;
+	}
+	else if(head == tail){
+		free(head);
+		head = tail = NULL;
+	}
+	else{
+		struct Node *temp = tail;
+		tail = tail->prev;
+		tail->next = NULL;
+		free(temp);
+	}
+	
 }
 
-void popMid(int data){
-    if(head == NULL){
-        return;
-    }
-    else if(head->data == data){
-        popHead();
-    }
-    else if(tail->data == data){
-        popTail();
-    }
+void popMid( char nama[101]){
+	
+	if(head == NULL){
+		return;
+	}
+	else if(strcmp(head->nama, nama) == 0){
+		popHead();
+	}
+	else if(strcmp(tail->nama, nama) == 0){
+		popTail();
+	}
+	else {
+		struct Node *curr = head;
+		while( (curr->next != NULL) && strcmp(curr->next->nama, nama) != 0){
+			curr = curr->next;
+		}
+		
+		if(curr->next != NULL){
+			struct Node *temp = curr->next;
+			curr->next = temp->next;
+			
+			if(temp->next != NULL){
+				temp->next->prev = curr;
+			}
+			
+			free(temp);
+		}
+		
+	}
+}
+
+void search(char nama[101]){
+	
+	struct Node *curr = head;
+	
+	while((curr != NULL) && strcmp(curr->nama,nama) != 0){
+		curr = curr->next;
+	}
+	
+	if(curr == NULL){
+		printf("%s wasn't found in the linked list!\n", nama);
+	}
+	else {
+		printf("%s was found in the linked list\n", nama);
+	}
+	
+}
+
+void print(){
+	
+	struct Node *curr = head;
+	while(curr != NULL){
+		printf("%s <-> ", curr->nama);
+		curr = curr->next;
+		
+	}
+	printf("NULL\n");
 }
 int main(){
-
+	
+	head = tail = NULL;
+	pushMid(newNode(1,"Alice"));
+    pushMid(newNode(2,"Bob"));
+    pushMid(newNode(3,"Charlie"));
+    pushMid(newNode(4,"David"));
+    pushMid(newNode(5,"Eve"));
+    pushMid(newNode(6,"Frank"));
+    pushMid(newNode(7,"Grace"));
+    pushMid(newNode(8,"Hannah"));
+    pushMid(newNode(9,"Isaac"));
+    print();
     
+    printf("\nAfter deleting Alice:\n");
+    popMid("Alice");
+    print();
+    
+    printf("\nSearching for Bob:\n");
+    search("Bob");
 
-
-
-
-    return 0;
+    printf("\nSearching for Alice:\n");
+    search("Alice");
+    
+	return 0;
 }
